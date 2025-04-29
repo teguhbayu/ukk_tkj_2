501b8ecce0e6e2240f1e6f9e72c0e272601e6cfb
Teguh Bayu Pratama <149038828+teguhbayu@users.noreply.github.com>
2025-04-29 14:11:02 +0800

    fix: patched the vuln :3

--- admin_login.php
+++ admin_login.php
@@ -5,9 +5,9 @@ include 'koneksi.php';
 
 if (isset($_POST['submit'])) {
 
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password =  $_POST['password'];
-	
+	$username = mysqli_real_escape_string($conn, $_POST['username']);
+	$password = mysqli_real_escape_string($conn, $_POST['password']);
+
 	$login = mysqli_query($conn, "SELECT * FROM user WHERE password = '$password' AND username = '$username' ");
 
 	if (mysqli_num_rows($login) == 0) {
@@ -16,12 +16,12 @@ if (isset($_POST['submit'])) {
 		$_SESSION['admin'] = 1;
 		header("Location: admin.php");
 	}
-	
+
 }
 
 //tambahkan atau perbaiki script diantara dibaris 7 sampai 12  
 //sql code injection  di login admin bagian pasword   ' OR 1=1 -- -' 
-	
+
 ?><!DOCTYPE html>
 <html lang="en">
 
@@ -31,24 +31,24 @@ if (isset($_POST['submit'])) {
 </head>
 
 <body>
-	
+
 	<h1 style="text-align: center">Login Admin</h1>
 	<hr>
-	
+
 	<form action="" method="post">
-	
+
 		<p>Username:</p>
 		<input type="text" name="username">
-		
+
 		<p>Password:</p>
 		<input type="password" name="password">
-		
+
 		<br>
 		<br>
 		<input type="submit" name="submit" value="Login">
-	
+
 	</form>
-	
+
 </body>
 
 </html>
